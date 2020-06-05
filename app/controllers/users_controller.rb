6a61ad23ee7360de60fbc47cfef324a15a5b667a
queeksm <queeksm@gmail.com>
2020-06-05 11:23:12 -0500

    Initial Linting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,39 @@
+# frozen_string_literal: true
+
+# Class
 class UsersController < ApplicationController
-    def new
-      @user = User.new()
+  def new
+    @user = User.new()
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def show
-      @user = User.find(params[:id])
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
+  end
 
-    def create
-  #    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
-    end
-
-    def edit
-        @user = User.find(params[:id])
-
-    end
-
-    def update
-        @user = User.find(params[:id])
-        @user.update(user_params)
-        if @user.save
-          redirect_to user_path(@user)
-        else
-          render :edit
-        end
-            
-    end 
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# Class
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates_length_of :username, :in => 4..12
-    validates :email, presence: true
-    validates :password, presence: true
+  validates :username, presence: true
+  validates_length_of :username, :in => (4..12)
+  validates :email, presence: true
+  validates :password, presence: true
 end
