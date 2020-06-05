999df9666640fa0a85253a66c49526932749e922
Stricks1 <gmalheiross@gmail.com>
2020-06-05 12:39:31 -0300

    edit partial without errors handlers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
       @user = User.new()
     end
 
+    def show
+      @user = User.find(params[:id])
+    end
+
     def create
   #    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user = User.new(user_params)
@@ -12,6 +16,23 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+    def edit
+        @user = User.find(params[:id])
+
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        @user2 = User.new(user_params)
+        unless @user2.valid?
+          redirect_to edit_user_url(@user) 
+        else
+          redirect_to user_path(@user)
+        end
+            
+    end 
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true
+    validates_length_of :username, :in => 4..12
     validates :email, presence: true
     validates :password, presence: true
 end
